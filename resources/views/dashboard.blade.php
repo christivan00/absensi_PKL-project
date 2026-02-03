99ce8979be0762e02bf85a7bfae3096afe719244
christivan00 <157097188+christivan00@users.noreply.github.com>
2026-02-03 21:51:12 +0700

    add

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>dashboard absensi</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container-fluid text-center bg-secondary row">
+        <div class="col">
+            <h1>kolom1</h1>
+        </div>
+        <div class="col">
+            <h1>kolom2</h1>
+        </div>
+    </div>
+    <footer class="container-fluid bg-secondary">
+        <h2 class="text-center">bagian bawah</h2>
+    </footer>
+</body>
+
+</html>
